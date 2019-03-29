cf97fa891a06894d9dd9dbfd6f68ebf3bcdbae0c
Contractor <wcontrator@gmail.com>
2019-03-29 14:37:18 -0400

    Updates

--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
 def add(first_term, second_term):
-    return first_term + second_term
+
+   return first_term + second_term
 
 def subtract(first_term, second_term):
+
     return first_term - second_term
+
+def multiply(first_term, second_term):
+    
+    return first_term * second_term
